c85f36dd8a1bca77ef26f2abf54251de8a6e7595
Kurtis Knodel <kurtisknodel@gmail.com>
2020-11-25 10:26:33 -0800

    Added project
    This is effectively release 0.1.0; which lacks some features and was built before adding source control to the project.

---
+++ Requester/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Requester
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSendRequestButtonClicked(object sender, RoutedEventArgs e)
+        {
+            string targetUrl = targetUrlInput.Text.Trim();
+            RequestMethod requestMethod = (RequestMethod)requestMethodInput.SelectedIndex;
+            ByteArrayContent requestBody = new ByteArrayContent(
+                Encoding.UTF8.GetBytes(requestBodyInput.Text)
+            );
+
+            SendRequest(targetUrl, requestMethod, requestBody, responseOutput, responseCodeLabel);
+        }
+
+        private async void SendRequest(string target, RequestMethod method, HttpContent body, TextBox contentOutput, Label statusCodeOutput)
+        {
+            RequestManager manager = RequestManager.Get();
+            RequestManager.Response response = await manager.SendRequest(MakeAbsolute(target), method, body);
+            contentOutput.Text = response.content;
+            statusCodeOutput.Content = $"Response Code: {response.statusCodeName} ({response.statusCode}).";
+        }
+
+        private string MakeAbsolute(string url)
+        {
+            if(url.StartsWith("https://") || url.StartsWith("http://"))
+            {
+                return url;
+            }
+            else
+            {
+                return $"https://{url}";
+            }
+        }
+    }
+}
---
+++ Requester/RequestManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Requester
+{
+    public enum RequestMethod
+    {
+        GET = 0,
+        POST, PUT,
+        DELETE,
+    }
+
+    public class RequestManager
+    { 
+        public struct Response
+        {
+            public int statusCode;
+            public string statusCodeName;
+            public string content;
+        }
+
+        private static RequestManager singleton = null;
+
+        public static RequestManager Get()
+        {
+            if(singleton == null)
+            {
+                singleton = new RequestManager();
+            }
+
+            return singleton;
+        }
+
+        private HttpClient client;
+
+        private RequestManager()
+        {
+            client = new HttpClient();
+        }
+
+        public async Task<Response> SendRequest(string target, RequestMethod method, HttpContent body)
+        {
+            HttpResponseMessage httpResponse = null;
+
+            try { 
+                switch (method)
+                {
+                    case RequestMethod.DELETE:
+                        httpResponse = await client.DeleteAsync(target); break;
+                    case RequestMethod.GET:
+                        httpResponse = await client.GetAsync(target); break;
+                    case RequestMethod.POST:
+                        httpResponse = await client.PostAsync(target, body); break;
+                    case RequestMethod.PUT:
+                        httpResponse = await client.PutAsync(target, body); break;
+                }
+            } catch(Exception e)
+            {
+                return new Response {
+                    statusCode = 0,
+                    statusCodeName = "ERROR",
+                    content = e.Message
+                };
+            }
+
+            Response response = new Response();
+
+            HttpStatusCode responseStatus = httpResponse.StatusCode;
+            response.statusCode = (int)responseStatus;
+            response.statusCodeName = responseStatus.ToString();
+
+            HttpContent responseContent = httpResponse.Content;
+            response.content = await responseContent.ReadAsStringAsync();
+
+            httpResponse.Dispose();
+            return response;
+        }
+    }
+}
